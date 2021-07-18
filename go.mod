144dea0cce78f2900b4ea4d86ea2d257c092b6fc
Aroldo D Michieletto Junior <juniormichieletto@gmail.com>
2021-07-18 20:43:41 +0200

    Add main temporary function to test db connection

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.io/junirmichieletto/codebank
 go 1.16
 
 require (
+	github.com/lib/pq v1.10.2 // indirect
 	github.com/satori/go.uuid v1.2.0
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
--- main.go
+++ main.go
@@ -1,5 +1,53 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"fmt"
+	"log"
 
+	_ "github.com/lib/pq"
+	"github.io/junirmichieletto/codebank/domain"
+	"github.io/junirmichieletto/codebank/infrastructure/repository"
+	"github.io/junirmichieletto/codebank/usecase"
+)
+
+func main() {
+	db := setupDb()
+	defer db.Close()
+
+	cc := domain.NewCreditCard()
+	cc.Number = "1234"
+	cc.Name = "Junior"
+	cc.ExpirationYear = 2021
+	cc.ExpirationMonth = 7
+	cc.CVV = 123
+	cc.Limit = 1000
+	cc.Balance = 0
+
+	repo := repository.NewTransactionRepositoryDB(db)
+	err := repo.CreateCreditCard(*cc)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func setupTransactionUseCase(db *sql.DB) usecase.TransactionUseCase {
+	transactionRepository := repository.NewTransactionRepositoryDB(db)
+	useCase := usecase.NewTransactionUseCase(transactionRepository)
+	return useCase
+}
+
+func setupDb() *sql.DB {
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		"localhost",
+		"5432",
+		"postgres",
+		"root",
+		"codebank",
+	)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatal("error connection to database")
+	}
+	return db
 }
